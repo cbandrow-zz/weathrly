abcf53a40ae85712989a4a69fa6a43b7c7cb0027
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-04-17 15:13:11 -0600

    test input stateful component. change styles. create new component.

---
+++ test/weathersummary.test.js
@@ -0,0 +1,12 @@
+import WeatherSummary from '../lib/components/WeatherSummary'
+import WeatherInput from '../lib/components/WeatherInput'
+import Weathrly from '../lib/components/Weathrly'
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+describe("Weather Input Tests", () =>{
+
+  it('should input text, and expect its state to change', () =>{
+    
+  })
+})
--- test/weathrly.test.js
+++ test/weathrly.test.js
@@ -3,13 +3,10 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 
 describe('Weathrly Application: ', () => {
-  it(' should test the state of App. ', () => {
+  it.only(' should test the state of App. ', () => {
 
     var wrapper = shallow( < Weathrly/> )
-
-    console.log(wrapper.debug())
-    //find input, add item, find button, click button, check state
-    //grabing input, simulating change by changing the event targe value
+    
 
   })
 })
